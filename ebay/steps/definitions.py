4f8d121a72054f9b48e66f8359af0ca8b7a47ecb
aedea <eddiegolv@gmail.com>
2024-05-10 16:18:06 -0700

    merged stashed & local changes

--- ebay/steps/definitions.py
+++ ebay/steps/definitions.py
@@ -15,9 +15,9 @@ def wait_for_element_by_xpath(driver, xpath, timeout=13):
             ec.visibility_of_element_located((By.XPATH, xpath))
         )
     except TimeoutException:
-        print("\033[91m !Timeout occurred: Element not found\033[0m")
+        print("\033[91m ❌ Timeout occurred: Element not found\033[0m")
     except Exception as e:
-        print("\033[91m !An error occurred:\033[0m", e)
+        print("\033[91m ❌ An error occurred:\033[0m", e)
 
 
 def compare_urls(driver, expected_url, name_of_element):
@@ -25,9 +25,9 @@ def compare_urls(driver, expected_url, name_of_element):
     # ! also put the name of the element being tested
     current_url = driver.current_url
     if current_url == expected_url:
-        print(name_of_element, " has successfully opened\n***")
+        print("✅", name_of_element, " has successfully opened\n***")
     else:
-        print("\033[91m", name_of_element, "has NOT opened !\033[0m\n***")
+        print("\033[91m❌", name_of_element, "has NOT opened !\033[0m\n***")
 
 
 def check_dropdown(driver, xpath, name_of_element):
@@ -35,9 +35,9 @@ def check_dropdown(driver, xpath, name_of_element):
     dropdown_exp = driver.find_element(By.XPATH, xpath)
     aria_exp_value = dropdown_exp.get_attribute("aria-expanded")
     if aria_exp_value == "true":
-        print(name_of_element, "has been expanded\n***")
+        print("✅", name_of_element, "has been expanded\n***")
     else:
-        print("\033[91m", name_of_element, "has NOT expanded !\033[0m\n***")
+        print("\033[91m❌", name_of_element, "has NOT expanded !\033[0m\n***")
 
 
 def attribute_to_be(locator, attribute, value):
@@ -52,7 +52,7 @@ def open_chrome(context):
     context.driver = webdriver.Chrome()
     context.wait = WebDriverWait(context.driver, 13)
     context.actions = ActionChains(context.driver)
-    print("Browser has successfully opened\n***")
+    print("✅ Browser has successfully opened\n***")
 
 
 @step('Navigate to eBay.com')
@@ -60,28 +60,28 @@ def navigate_to_ebay(context):
     context.driver.get("https://ebay.com")
     #   waiting until logo has loaded
     wait_for_element_by_xpath(context.driver, "//img[@id='gh-logo' and @alt='eBay Home']")
-    print("Navigated to ebay.com\n***")
+    print("✅ Navigated to ebay.com\n***")
 
 
 @step('Enter "dress" to the searchbar')
 def enter_dress(context):
     searchbar = context.driver.find_element(By.XPATH, "//input[@aria-label='Search for anything']")
     searchbar.send_keys("dress")
-    print("Entered 'dress' into the search bar\n***")
+    print("✅ Entered 'dress' into the search bar\n***")
 
 
 @step('Click on "Search" button')
 def click_search_button(context):
     search_button = context.driver.find_element(By.XPATH, "//input[@id='gh-btn']")
     search_button.click()
-    print("Clicked on 'Search' button\n***")
+    print("✅ Clicked on 'Search' button\n***")
     #   waiting until 1st dress is loaded and visible
     context.first_dress = context.wait.until(
         ec.visibility_of_element_located(
             (By.XPATH, "(//div[@class='s-item__image-wrapper image-treatment'])[2]")
         )
     )
-    print("Page has loaded\n***")
+    print("✅ Results page has loaded\n***")
 
 
 @step('Click on the first dress')
@@ -89,27 +89,28 @@ def click_first_dress(context):
     initial_handles = context.driver.window_handles
     current_handle = context.driver.current_window_handle
     context.first_dress.click()
-    print("Clicked on the first dress\n***")
+    print("✅ Clicked on the first dress\n***")
     updated_handles = context.driver.window_handles
     if len(updated_handles) > len(initial_handles):
-        print("A new tab has opened.\n***")
+        print("✅ A new tab with the product has opened\n***")
     else:
-        print("No new tab has been opened.\n***")
+        print("❌ No new tab has been opened\n***")
     #   switching to a newly opened tab
     for handle in context.driver.window_handles:
         if handle != current_handle:
             context.driver.switch_to.window(handle)
             break
-    print("Opened product:", context.driver.title, "\n***")
+    print("✅ Opened product:", context.driver.title, "\n***")
     context.wait.until(
         ec.visibility_of_element_located(
             (By.XPATH, "//a[@id='binBtn_btn_1']")
         )
     )
-    print("'Buy It Now' button is shown, page is loaded\n***")
+    print("✅ Product page has loaded\n***")
     product_title_el = context.driver.find_element(
         By.XPATH, "//h1[@class='x-item-title__mainTitle']/span"
     )
+    # storing product title to compare in the next step if this item was added to the cart
     context.product_title = product_title_el.text
 
 
@@ -119,7 +120,7 @@ def click_add_to_cart_button(context):
         By.XPATH, "//a[@id='atcBtn_btn_1']"
     )
     context.add_to_cart_btn.click()
-    print("Clicked 'Add to cart' button\n***")
+    print("✅ Clicked 'Add to cart' button\n***")
     wait_for_element_by_xpath(context.driver, "//div[@class='ux-layout-section__row']//span[text()='Go to cart']")
     """
     context.wait.until(
@@ -128,7 +129,7 @@ def click_add_to_cart_button(context):
         )
     )
     """
-    print("Lightbox has opened\n***")
+    print("✅ Lightbox has opened, product has been added to the cart\n***")
 
 
 @step('Click "Go to cart"')
@@ -137,13 +138,13 @@ def click_go_to_cart(context):
         By.XPATH, "//div[@class='ux-layout-section__row']//span[text()='Go to cart']"
     )
     context.go_to_cart_btn.click()
-    print("Clicked 'Go to cart' button\n***")
+    print("✅ Clicked 'Go to cart' button\n***")
     context.wait.until(
         ec.visibility_of_element_located(
             (By.XPATH, "//button[text()='Go to checkout']")
         )
     )
-    print("Cart is opened\n***")
+    print("✅ Cart has opened\n***")
 
 
 @step('Verify the item was added to the cart')
@@ -152,11 +153,11 @@ def click_sell_button(context):
         context.wait.until(
             ec.text_to_be_present_in_element((By.XPATH, "//div[@data-test-id='app-cart']"), context.product_title)
         )
-        print("Item was successfully added to the cart")
+        print("✅ Item was successfully added to the cart")
     except TimeoutException:
-        print("Timeout\n**")
+        print("❌ Timeout\n**")
     except Exception as e:
-        print(e)
+        print("❌", e)
     context.driver.quit()
 
 
@@ -165,61 +166,61 @@ def click_sell_button(context):
 def click_sign_in(context):
     context.sign_in_btn = context.driver.find_element(By.XPATH, "//span[@id='gh-ug']/a[text()='Sign in']")
     context.sign_in_btn.click()
-    print("Clicked 'Sign in' button\n***")
+    print("✅ Clicked 'Sign in' button\n***")
     try:
         context.wait.until(
             ec.presence_of_element_located((By.XPATH, "//input[@id='userid']"), )
         )
-        print("Sign in page has successfully opened\n***")
+        print("✅ Sign in page has successfully opened\n***")
     except TimeoutException:
-        print("Timeout\n**")
+        print("❌Timeout\n**")
     except Exception as e:
-        print(e)
+        print("❌", e)
 
 
 @step('Click "register" and verify registration page has opened')
 def click_register(context):
     context.register_btn = context.driver.find_element(By.XPATH, "//span[@id='gh-ug-flex']/a[text()='register']")
     context.register_btn.click()
-    print("Clicked 'register' button\n***")
+    print("✅ Clicked 'register' button\n***")
     try:
         context.wait.until(
             ec.presence_of_element_located((By.XPATH, "//button[@id='EMAIL_REG_FORM_SUBMIT']"))
         )
-        print("Registration page has successfully opened\n***")
+        print("✅ Registration page has successfully opened\n***")
     except TimeoutException:
-        print("Timeout\n**")
+        print("❌ Timeout\n**")
     except Exception as e:
-        print(e)
+        print("❌", e)
 
 
 @step('Click "Daily Deals" and verify daily deals page has opened')
 def click_daily_deals(context):
     context.daily_deals_btn = context.driver.find_element(By.XPATH, "//a[text()=' Daily Deals' and @class='gh-p']")
     context.daily_deals_btn.click()
-    print("Clicked 'Daily Deals' button\n***")
+    print("✅ Clicked 'Daily Deals' button\n***")
     expected_url = "https://www.ebay.com/deals"
     current_url = context.driver.current_url
     if current_url == expected_url:
-        print("Daily Deals page has successfully opened\n***")
+        print("✅ Daily Deals page has successfully opened\n***")
     else:
-        print("Daily Deals page is not opened\n***")
+        print("❌ Daily Deals page is not opened\n***")
 
 
 @step('Click "Brand Outlet" and verify Brand Outlet page has opened')
 def click_brand_outlet(context):
     context.brand_outlet_link = context.driver.find_element(By.XPATH, "//li[@id='gh-p-4']/a")
     context.brand_outlet_link.click()
-    print("Clicked 'Brand Outlet' link\n***")
+    print("✅ Clicked 'Brand Outlet' link\n***")
     try:
         context.wait.until(
             ec.presence_of_element_located((By.XPATH, "//a[@class='seo-breadcrumb-text']/span[text()='Brand Outlet']"))
         )
-        print("Brand Outlet page has successfully opened\n***")
+        print("✅ Brand Outlet page has successfully opened\n***")
     except TimeoutException:
-        print("Timeout\n**")
+        print("❌ Timeout\n**")
     except Exception as e:
-        print(e)
+        print("❌", e)
 
 
 @step('Click "Gift Cards" and verify gift cards page has opened')
@@ -228,7 +229,7 @@ def click_gift_cards(context):
         By.XPATH, "//a[contains(text(), 'Gift Cards') and @class='gh-p']"
     )
     context.gift_cards_btn.click()
-    print("Clicked 'Gift Cards' button\n***")
+    print("✅ Clicked 'Gift Cards' button\n***")
     compare_urls(context.driver, "https://www.ebay.com/giftcards", "Gift Cards page")
 
 
@@ -238,7 +239,7 @@ def click_help(context):
         By.XPATH, "//a[contains(text(),'Help & Contact') and @class='gh-p']"
     )
     context.help_contact_btn.click()
-    print("Clicked 'Help & Contact' button\n***")
+    print("✅ Clicked 'Help & Contact' button\n***")
     wait_for_element_by_xpath(context.driver, "//a[@aria-current='location' and text()='Customer Service']")
     compare_urls(context.driver, "https://www.ebay.com/help/home", "Help & Contact page")
 
@@ -247,7 +248,7 @@ def click_help(context):
 def click_sell(context):
     context.sell_btn = context.driver.find_element(By.XPATH, "//a[contains(text(),'Sell') and @class='gh-p']")
     context.sell_btn.click()
-    print("Clicked 'Sell' button\n***")
+    print("✅ Clicked 'Sell' button\n***")
     compare_urls(context.driver, "https://www.ebay.com/sl/sell", "Sell page")
 
 
@@ -297,7 +298,7 @@ def verify_dropdown_element(context, dropdown_element):
 def hover_my_ebay(context):
     my_ebay_dd = context.driver.find_element(By.XPATH, "//a[@title='My eBay']")
     context.actions.move_to_element(my_ebay_dd).perform()
-    print("Hovered over My eBay dropdown\n***")
+    print("✅ Hovered over My eBay dropdown\n***")
     wait_for_element_by_xpath(context.driver, "//ul[@id='gh-ul-nav']/li/a[contains(text(),'Summary')]")
     check_dropdown(context.driver, "//a[text()='Expand My eBay']", "My eBay dropdown")
 
@@ -306,7 +307,7 @@ def hover_my_ebay(context):
 def hover_notification(context):
     context.notifications_icon = context.driver.find_element(By.XPATH, "//i[@id='gh-Alerts-i']//parent::button")
     context.actions.move_to_element(context.notifications_icon).perform()
-    print("Hovered over notifications icon\n***")
+    print("✅ Hovered over notifications icon\n***")
     wait_for_element_by_xpath(context.driver, "//span[@class='ghn-errb ghn-errb-a']/a/span")
     check_dropdown(context.driver, "//i[@id='gh-Alerts-i']//parent::button", "Notifications")
 
@@ -315,6 +316,16 @@ def hover_notification(context):
 def verify_cart(context):
     cart_icon = context.driver.find_element(By.XPATH, "//li[@id='gh-minicart-hover']")
     cart_icon.click()
-    print("Clicked on cart icon\n***")
+    print("✅ Clicked on cart icon\n***")
     wait_for_element_by_xpath(context.driver, "//h1[@data-test-id='main-title' and text()='Shopping cart']")
     compare_urls(context.driver, "https://cart.ebay.com/", "Cart page")
+
+
+"""
+@step('Click "{box}"')
+def header_navigation(context, box):
+    context.nav_button = context.driver.find_element(
+        By.XPATH, f"//*[contains(@class, ‘gh-’) and normalize-space(text()) = ‘{box}’]"
+    )
+    context.dd_btn.click()
+"""
