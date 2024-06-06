75b650feb74885bab1d4ff8dc6ca67a5ceed83f2
aedea <eddiegolv@gmail.com>
2024-06-06 13:09:35 -0700

    added case insensetivity for validation and issue counts

--- ebay/steps/6th_test-extended_filter_validation.py
+++ ebay/steps/6th_test-extended_filter_validation.py
@@ -3,7 +3,6 @@
 
 from behave import step
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as ec
 
 
@@ -11,45 +10,52 @@ from selenium.webdriver.support import expected_conditions as ec
 def filter_validation(context):
     all_items = context.driver.find_elements(By.XPATH, "//li[contains(@id, 'item')][//span[@role='heading']]")
     initial_window = context.driver.current_window_handle  # getting id of the current tab
-    # issues = []
-    cycle_issues = []
+    issues = []
+    all_issues = []
     item_count = 0
     issue_count = 0
     for item in all_items:
         item_count += 1
-        title = item.find_element(By.XPATH, ".//span[@role='heading']").text
+        # title = item.find_element(By.XPATH, ".//span[@role='heading']").text
         product_url = item.find_element(By.XPATH, ".//a[@class='s-item__link']").get_attribute('href')
         # get to the item page
-        context.driver.execute_script(f"window.open('{product_url}');")  # open urls from product_url
-        context.driver.switch_to.window(context.driver.window_handles[-1])  # switch to the latest tab
-        # collect item spec
+        context.driver.execute_script(f"window.open('{product_url}');")  # opening urls from product_url
+        context.driver.switch_to.window(context.driver.window_handles[-1])  # switching to the latest tab
+        # collecting item specs
         item_labels = context.wait.until(
             ec.presence_of_all_elements_located((
                 By.XPATH, "//dt[@class='ux-labels-values__labels']//span")))
         item_values = context.wait.until(
             ec.presence_of_all_elements_located((
                 By.XPATH, "//dd[@class='ux-labels-values__values']//div[@class='ux-labels-values__values-content']")))
-        # get text from items
+        # getting text from items and creating dictionary
         item_labels_text = []
         for label in item_labels:
             item_labels_text.append(label.text)
         item_values_text = []
         for value in item_values:
             item_values_text.append(value.text)
-        item_specs = dict(zip(item_labels_text, item_values_text))  # dict        # the validation
-        if context.filter_section or context.filter_subsection not in item_specs.keys():
-            # issues.append(f"{title} doesnt have anything related to {context.filter_section}")
-            cycle_issues.append(f"doesnt have anything related to {context.filter_section}")
-        if item_specs[context.filter_section] != context.filter_value:
-            cycle_issues.append(f"doesnt have anything related to {context.filter_section}")
-            # issues.append(f" Not related to {context.filter_value} by {context.filter_section} "
-
-        print("🛈 Checked product №", item_count, "/", len(all_items))
-        if cycle_issues:
-            print(f'Title:, {title} \n URL: {product_url} \n Found issues:\n{"\n".join(cycle_issues)}')
-        cycle_issues = []
-        # close tab
+        item_specs = dict(zip(item_labels_text, item_values_text))
+        # validation
+        if context.filter_section.lower() not in [k.lower() for k in item_specs.keys()]:
+            issue_count += 1
+            issues.append(f"The product doesn't have '{context.filter_section}' specification")
+            all_issues.append(f"{issue_count}) The product doesn't have '{context.filter_section}' specification\n"
+                              f"Product URL: {product_url}\n")
+        elif item_specs[context.filter_section].lower() != context.filter_value.lower():
+            issue_count += 1
+            issues.append(f"The '{context.filter_section}' doesn't match '{context.filter_value}'. "
+                          f"Actual value: '{item_specs[context.filter_section]}'")
+            all_issues.append(f"{issue_count}) The '{context.filter_section}' doesn't match '{context.filter_value}'. "
+                              f"Actual value: '{item_specs[context.filter_section]}'\n"
+                              f"Product URL: {product_url}\n")
+        print("🛈 Checked product №", item_count, "/", len(all_items), ": ", end='')
         context.driver.close()
         context.driver.switch_to.window(initial_window)
-    # if issues:
-    #     raise Exception(f'Following issues discovered:\n{"\n".join(issues)}')
+        if issues:
+            print(f'❌\nFound issues:\n{"\n".join(issues)}\nURL: {product_url}')
+        else:
+            print("✅")
+        issues = []
+    if all_issues:
+        raise Exception(f'Found {issue_count} issues:\n{"\n".join(all_issues)}')
